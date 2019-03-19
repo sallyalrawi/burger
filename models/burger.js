30ba6e2032842d476003de31fc3f7ed658e258d3
sally <sallyakrum83@gmail.com>
2019-03-19 13:29:25 -0500

    added layouts

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,18 @@
-var orm = require("../config/orm.js");
+var orm = require("../config/orm.js"); 
 
 var burger = {
-    selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
-      cb(res);
-    });
-  },
- 
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      console.log("**burger js insertOne test**");
-      cb(res);
-    });
-  },
-  updateOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
-      console.log("**burger js updateOne test**");
-      cb(res);
-    });
-  }
- 
-};
+  selectAll: function(cb) {
+        orm.selectAll("burgers", function(res){
+            cb(res); 
+        })
+    },
+    updateOne: function(id,cb) {
+            orm.updateOne("burgers", id, cb);
 
-// Export the database functions.
-module.exports = burger;
+        }, 
+        insertOne: function(name,cb){
+            orm.insertOne("burgers", name,cb);
+        }
+    }
+
+module.exports = burger; 
