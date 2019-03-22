a61fb75d764f80381f72188cf595fd3624b589a9
sally <sallyakrum83@gmail.com>
2019-03-22 11:27:36 -0500

    added some styles

--- public/assests/js/burgerjs.js
+++ public/assests/js/burgerjs.js
@@ -1,17 +1,20 @@
+$(document).ready(function(){
+  $(".devourBtn").on("submit", function(event) {
+    event.preventDefault();
+    // console.log($(this).children('.burger_id').val());
+    var id = $(this).children('.burger_id').val();
+  //   var newBurger = $(this).data("newburger");
 
-    $(".btn btn-default").on("submit", function(event) {
-        event.preventDefault();
-        var id = $(this).data("id");
-        var newBurger = $(this).data("newburger");
-  
-        var newBurgerState = {
-            burger: newBurger
-      };
-      $.ajax({
-        method: "PUT",
-        url: "/burgers/" + id + newBurgerState
-      }).then(function(data) {
-        // reload page to display devoured burger in proper column
-        location.reload();
+  //   var newBurgerState = {
+  //       burger: newBurger
+  // };
+  $.ajax({
+    method: "PUT",
+    url: "/burgers/update/" + id 
+  }).then(function(data) {
+    // reload page to display devoured burger in proper column
+    location.reload();
+  });
       });
-          });
+});
+   
