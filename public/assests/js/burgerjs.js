96fc968165835b7a33f5136a8a7c4c3336172e55
sally <sallyakrum83@gmail.com>
2019-03-22 22:34:28 -0500

    added some changes

--- public/assests/js/burgerjs.js
+++ public/assests/js/burgerjs.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     event.preventDefault();
     // console.log($(this).children('.burger_id').val());
     var id = $(this).children('.burger_id').val();
-  //   var newBurger = $(this).data("newburger");
+    var newBurger = $(this).data("newburger");
 
-  //   var newBurgerState = {
-  //       burger: newBurger
-  // };
+    var newBurgerState = {
+        burger: newBurger
+  };
   $.ajax({
     method: "PUT",
-    url: "/burgers/update/" + id 
+    url: "/burgers/update/" + id + newBurgerState
   }).then(function(data) {
     // reload page to display devoured burger in proper column
     location.reload();
